2ffa92574daf72c1708b1e4a844bb2c83d00732e
Artem Shashkov <majento@bk.ru>
2023-02-15 16:29:43 +0300

    add: eslintrc update: Makefile

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const greetings = () => {
-const name = readlineSync.question(`May I have your name?`);
-console.log(`Hello, ${name}!`);
+export default function greetings() {
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
 }
-export { greetings };
